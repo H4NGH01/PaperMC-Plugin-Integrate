32761dd0d2a1028314ef1a83407fc56c71a881e5
H4NGH01 <imc84668@gmail.com>
2022-02-04 01:05:07 +0800

    Add files via upload

--- src/main/java/me/core/cases/CaseKey.java
+++ src/main/java/me/core/cases/CaseKey.java
@@ -1,7 +1,29 @@
 package me.core.cases;
 
-public class CaseKey {
+public enum CaseKey {
 
+    KEY_WEAPON_CASE("key_weapon_case", "Weapon Case Key", CaseType.WEAPON_CASE);
 
+    private final String id;
+    private final String name;
+    private final CaseType canOpen;
+
+    CaseKey(String id, String name, CaseType canOpen) {
+        this.id = id;
+        this.name = name;
+        this.canOpen = canOpen;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public CaseType canOpen() {
+        return this.canOpen;
+    }
 
 }
